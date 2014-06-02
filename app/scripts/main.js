2407b7b531e49bd3adf5a2bc33592f83f517a9b4
Fabio Albertin <albertin@gmail.com>
2014-06-03 01:06:37 +0200

    Fixed jQuery syntax errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,34 +5,34 @@ var date = timeSince(new Date);
 $(document).ready(function() {
     $('#submit-new-article').on('click', onClick);
 
-    var addImgValToClonedArticle = function(article){
+    var addImgValToClonedArticle = function (article) {
         var imgVal = $('#img').val();
-        if(imgVal.length > 0){
+        if (imgVal.length > 0) {
             article.find('img').attr('src', imgVal);
-        };
+        }
         return article;
     };
 
-    var addCaptionValToClonedArticle = function(article){
+    var addCaptionValToClonedArticle = function (article) {
         var captionVal = $('#caption').val();
-        if(captionVal.length > 0){
+        if (captionVal.length > 0) {
             article.find('h4').text(captionVal);
 
-        };
+        }
         return article;
     };
 
-    var addHrefValToClonedArticle = function(article){
+    var addHrefValToClonedArticle = function (article) {
         var hrefVal = $('#href').val();
-        if(hrefVal.length > 0){
+        if (hrefVal.length > 0) {
             article.find('.srclink').attr('href', hrefVal).text('(' + hrefVal + ')');
-        };
+        }
         return article;
     };
 
-    function onClick(){
-        var article = $('#articles article').first()
-        var newArticle = article.clone()
+    function onClick() {
+        var article = $('#articles article').first();
+        var newArticle = article.clone();
 
         newArticle = addImgValToClonedArticle(newArticle);
         newArticle = addCaptionValToClonedArticle(newArticle);
@@ -47,23 +47,34 @@ $(document).ready(function() {
 // console.log(noArticles);
 
 // source: http://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
-function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var interval = Math.floor(seconds / 31536000);
+    function timeSince(date) {
+        var seconds = Math.floor((new Date() - date) / 1000);
+        var interval = Math.floor(seconds / 31536000);
 
-    if (interval > 1) { return interval + " years"; }
+        if (interval > 1) {
+            return interval + " years";
+        }
 
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) { return interval + " months"; }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
 
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) { return interval + " days"; }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
 
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) { return interval + " hours"; }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
 
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) { return interval + " minutes"; }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
 
-    return Math.floor(seconds) + " seconds";
-}
+        return Math.floor(seconds) + " seconds";
+    }
+});
