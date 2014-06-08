285c3497d4c6cd5ef479a55bc5f65b14116691ab
Fabio Albertin <albertin@gmail.com>
2014-06-09 01:05:48 +0200

    New structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,45 +4,9 @@ $(document).ready(function() {
     var date = timeSince(new Date());
     var noArticles = 1;
 
-    var addImgValToClonedArticle = function (article) {
-        var imgVal = $('#img').val();
-        if (imgVal.length > 0) {
-            article.find('img').attr('src', imgVal);
-        }
-        return article;
-    };
-
-    var addCaptionValToClonedArticle = function (article) {
-        var captionVal = $('#caption').val();
-        if (captionVal.length > 0) {
-            article.find('h4').text(captionVal);
-
-        }
-        return article;
-    };
-
-    var addHrefValToClonedArticle = function (article) {
-        var hrefVal = $('#href').val();
-        if (hrefVal.length > 0) {
-            article.find('.srclink').attr('href', hrefVal).text('(' + hrefVal + ')');
-        }
-        return article;
-    };
-
-    function onClick() {
-        var article = $('#articles article').first();
-        var newArticle = article.clone();
-
-        newArticle = addImgValToClonedArticle(newArticle);
-        newArticle = addCaptionValToClonedArticle(newArticle);
-        newArticle = addHrefValToClonedArticle(newArticle);
-        noArticles++;
-        $(newArticle).attr('id', 'article-0' + noArticles);
-
-        $(article).before(newArticle);
-
-        return false;
-    }
+    var imgVal = $('#img').val();
+    var captionVal = $('#caption').val();
+    var hrefVal = $('#href').val();
 
     /*
         Creating article list,
@@ -52,10 +16,11 @@ $(document).ready(function() {
     var articleList = [];
     var defaultRank = 1;
     // populate articleList with existing content
-    function frontArticle(text,link) {
+    function frontArticle(text,link,image) {
         this.id = noArticles++;
         this.rank = defaultRank;
         this.text = text;
+        this.image = image;
         this.link = link;
         this.comments = [];
     }
@@ -65,6 +30,10 @@ $(document).ready(function() {
 
     articleList.push(article01);
     articleList.push(article02);
+
+    function submitArticle(){
+        articleList.push(new frontArticle(captionVal,hrefVal,imgVal));
+    }
 
     /* render when:
         1. Page first loads
@@ -86,39 +55,7 @@ $(document).ready(function() {
         $('#articles').html(order);
     }
 
-    $('#submit-new-article').on('click', onClick);
+    $('#submit-new-article').on('click', submitArticle);
+
 // Check on noArticles after use
 // console.log(noArticles);
-
-// source: http://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
-    function timeSince(date) {
-        var seconds = Math.floor((new Date() - date) / 1000);
-        var interval = Math.floor(seconds / 31536000);
-
-        if (interval > 1) {
-            return interval + ' years';
-        }
-
-        interval = Math.floor(seconds / 2592000);
-        if (interval > 1) {
-            return interval + ' months';
-        }
-
-        interval = Math.floor(seconds / 86400);
-        if (interval > 1) {
-            return interval + ' days';
-        }
-
-        interval = Math.floor(seconds / 3600);
-        if (interval > 1) {
-            return interval + ' hours';
-        }
-
-        interval = Math.floor(seconds / 60);
-        if (interval > 1) {
-            return interval + ' minutes';
-        }
-
-        return Math.floor(seconds) + ' seconds';
-    }
-});
