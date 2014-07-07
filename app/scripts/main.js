5eca949b70a26624ec1bd7356a5cb265225abcd8
Franca Rast <franca.rast@screenconcept.ch>
2014-07-07 17:29:23 +0200

    remove console.log stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -68,11 +68,9 @@ jQuery(document).ready(function($) {
 
             if($(this).hasClass('up')){
                 newVal = oldVal + 1;
-                console.log('up ' + cRid + ' newVal is ' + newVal);
             }
             else if ($(this).hasClass('down')){
                 newVal = oldVal - 1;
-                console.log('down ' + cRid + ' newVal is ' + newVal);
             }
 
             // TODO: .score should display actual number of votes
@@ -192,7 +190,6 @@ jQuery(document).ready(function($) {
     function submitComment() {
       var cmntVal = $('#cmnt').val();
       var articleId = getUrlVars().id.replace('#', '');
-      console.log(articleId);
       var article = articleList[articleId];
 
       if (article !== undefined && cmntVal.length > 0) {
